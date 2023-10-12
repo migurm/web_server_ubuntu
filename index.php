d646dc68608842099f9be4af2b94b7dd2c832ae7
Miguel Angel Rueda Moreno <migue.rueda.90@gmail.com>
2023-10-12 22:35:43 +0200

    desde vsc

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <link rel = "stylesheet" type = "text/css" href = "css/estilo_index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ubuntu_serversito</title>
     <?php require "./paginas/libreria_funciones.php"?>
+    <link rel = "stylesheet" type = "text/css" href = "css/estilo_index.css">
 </head>
 <body>
+    <header>
+        <h1>server_ubuntu</h1>
+        <h2>serversito</h2>
+    </header>
+    <nav>
+
+    </nav>
+    <main>
+
+    </main>
+    <aside>
+
+    </aside>
+    <footer>
+
+    </footer>
+
     <h1>serversito</h1>
     <h3>ubuntu server</h3>
     <?php saludo() ?>
